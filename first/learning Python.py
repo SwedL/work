90e6d3d921bdfd526292e4c1b3e8152bf747d15b
AL <swedl@yandex.ru>
2022-09-15 18:09:11 +0500

    Гуру прогрессий

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,8 +1,4 @@
 
 import sys
 
-data = list(map(int, map(str.strip, sys.stdin)))
-for player in data[::-1]:
-    if player % 2 == 0:
-        print(['Арни', 'Дима'][data.index(player)%2 == 0])
-        break
+data = [int(i.strip()) for i in sys.stdin]
