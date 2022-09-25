60a81ee431a090831c8e74c098a8176b41d73b62
AL <swedl@yandex.ru>
2022-09-25 17:21:08 +0500

    Учим CSV 3 -2

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,9 +1,13 @@
 
 import csv
 
-def condense_csv()
+with open('D:/student_counts.csv', 'r', encoding='utf-8') as inp_data:
+    data = list(csv.DictReader(inp_data))
+    header = [k for k in data[0].keys()]
 
-
+    for line in data:
+        print(line)
+    print(header)
 
 
 
