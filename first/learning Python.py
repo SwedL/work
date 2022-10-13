6bb37245fbc9a7e8824cd00d80b1520ac19238cd
Алексей <supersega@mail.ru>
2022-10-13 23:02:54 +0500

    учим namedtuple

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,11 +1,35 @@
 
-n = (1, 2)
-c = (3, 4)
-p = n + c
-print(p)
+from collections import namedtuple
 
+User = namedtuple('User', ['name', 'surname', 'email', 'plan'])
 
+users = [User('Mary', 'Griffin', 'sonnen@yahoo.com', 'Basic'),
+         User('Brenda', 'Young', 'retoh@outlook.com', 'Silver'),
+         User('Kathleen', 'Lyons', 'balchen@att.net', 'Gold'),
+         User('Pamela', 'Hicks', 'corrada@sbcglobal.net', 'Silver'),
+         User('William', 'Townsend', 'kosact@verizon.net', 'Gold'),
+         User('Clayton', 'Morris', 'berserk@yahoo.com', 'Silver'),
+         User('Dorothy', 'Dennis', 'sequin@live.com', 'Gold'),
+         User('Tyler', 'Walker', 'noahb@comcast.net', 'Basic'),
+         User('Joseph', 'Moore', 'ylchang@sbcglobal.net', 'Silver'),
+         User('Kenneth', 'Richardson', 'tbusch@me.com', 'Bronze'),
+         User('Stephanie', 'Bush', 'neuffer@live.com', 'Gold'),
+         User('Gregory', 'Hughes', 'juliano@att.net', 'Basic'),
+         User('Tracy', 'Wallace', 'sblack@me.com', 'Silver'),
+         User('Russell', 'Smith', 'isaacson@comcast.net', 'Bronze'),
+         User('Megan', 'Patterson', 'hoangle@outlook.com', 'Basic')]
 
+list_result, plan_dict = [], {'Gold': 1, 'Silver': 2, 'Bronze': 3, 'Basic': 4}
+
+for k in users:
+    plan_key = k[3]
+    tmp = k._asdict()
+    tmp.setdefault('status', plan_dict[plan_key])
+    list_result.append(tmp)
+
+for i in sorted(list_result, key=lambda x: (x['status'], x['email'])):
+    print(f"{i['name']} {i['surname']}\n  Email: {i['email']}\n  Plan: {i['plan']}")
+    print()
 
 
 
