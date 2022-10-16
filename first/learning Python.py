ebef4a38bda38481587fc0ffd73d125325794244
AL <swedl@yandex.ru>
2022-10-16 17:02:53 +0500

    Учим defaultdict последний тест

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,8 +1,24 @@
-
 from collections import defaultdict
 
-staff_broken = [('Developing', 'Miguel Norris'), ('Sales', 'Connie Reid'), ('Sales', 'Joseph Lee'), ('Marketing', 'Carol Peters'), ('Accounting', 'Linda Hudson'), ('Accounting', 'Ann Bell'), ('Marketing', 'Ralph Morgan'), ('Accounting', 'Gloria Higgins'), ('Developing', 'Wilma Woods'), ('Developing', 'Wilma Woods'), ('Marketing', 'Bernice Ramos'), ('Marketing', 'Joyce Lawrence'), ('Accounting', 'Craig Wood'), ('Developing', 'Nicole Watts'), ('Sales', 'Jose Taylor'), ('Accounting', 'Linda Hudson'), ('Accounting', 'Edna Cunningham'), ('Sales', 'Jose Taylor'), ('Marketing', 'Helen Taylor'), ('Accounting', 'Kimberly Reynolds'), ('Marketing', 'Mary King'), ('Sales', 'Joseph Lee'), ('Accounting', 'Gloria Higgins'), ('Marketing', 'Andrew Clark'), ('Accounting', 'John Watts'), ('Accounting', 'Rosemary Garcia'), ('Accounting', 'Steven Diaz'), ('Marketing', 'Mary King'), ('Sales', 'Gladys Taylor'), ('Developing', 'Thomas Porter'), ('Accounting', 'Brenda Davis'), ('Sales', 'Connie Reid'), ('Sales', 'Alicia Mendoza'), ('Marketing', 'Mario Reynolds'), ('Sales', 'John White'), ('Developing', 'Joyce Rivera'), ('Accounting', 'Steven Diaz'), ('Developing', 'Arlene Gibson'), ('Sales', 'Robert Barnes'), ('Sales', 'Charlotte Cox'), ('Accounting', 'Craig Wood'), ('Marketing', 'Carol Peters'), ('Marketing', 'Ralph Morgan'), ('Accounting', 'Kay Scott'), ('Sales', 'Evelyn Martin'), ('Marketing', 'Billy Lloyd'), ('Sales', 'Gladys Taylor'), ('Developing', 'Deborah George'), ('Sales', 'Charlotte Cox'), ('Marketing', 'Sam Davis'), ('Sales', 'John White'), ('Sales', 'Marie Cooper'), ('Marketing', 'John Gonzalez'), ('Sales', 'John Washington'), ('Sales', 'Chester Fernandez'), ('Sales', 'Alicia Mendoza'), ('Sales', 'Katie Warner'), ('Accounting', 'Jane Jackson'), ('Sales', 'Chester Fernandez'), ('Marketing', 'Charles Bailey'), ('Marketing', 'Gail Hill'), ('Accounting', 'Casey Jenkins'), ('Accounting', 'James Wilkins'), ('Accounting', 'Casey Jenkins'), ('Marketing', 'Mario Reynolds'), ('Accounting', 'Aaron Ferguson'), ('Accounting', 'Kimberly Reynolds'), ('Sales', 'Robert Barnes'), ('Accounting', 'Aaron Ferguson'), ('Accounting', 'Jane Jackson'), ('Developing', 'Deborah George'), ('Accounting', 'Michelle Wright'), ('Accounting', 'Dale Houston')]
 
-department_staff = defaultdict(list)
-[department_staff[otdel].append(personal) for otdel, personal in staff_broken]
-[print(f'{k}: {len(department_staff[k])}') for k in sorted(department_staff)]
+# def best_sender(messages, senders):
+#     message_sender_dict = defaultdict(int)
+#     for s, m in zip(senders, messages):
+#         if message_sender_dict[s] < len(m.split()):
+#             message_sender_dict[s] = len(m.split())
+#     list_result = [key for key, values in message_sender_dict.items() if values == message_sender_dict[sorted(message_sender_dict, key=lambda k: (message_sender_dict[k]))[-1]]]
+#     return sorted(list_result)
+
+def best_sender(messages, senders):
+    message_sender_dict = defaultdict(int)
+    for s, m in zip(senders, messages):
+        if message_sender_dict[s] < len(m.split()):
+            message_sender_dict[s] = len(m.split())
+    return sorted(message_sender_dict, key=lambda k: (message_sender_dict[k], k))
+
+messages = ['Hello userTwooo', 'Hi userThree', 'Wonderful day Alice', 'Nice day userThree']
+senders = ['Alice', 'userTwo', 'userThree', 'Alice']
+
+print(best_sender(messages, senders))
+
+
