b2756ce0da9bb7322317969e97791d0ede08f9db
Алексей <supersega@mail.ru>
2022-10-17 22:17:50 +0500

    учим OrderedDict

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,22 +1,21 @@
 from collections import OrderedDict
 
-def custom_sort(ordered_dict, by_values=False):
-    key_res = []
-    key_res = [sorted(ordered_dict, key=lambda ord: ord[0]), sorted(ordered_dict, key=lambda ord: ord[1])][by_values]
-    print(key_res)
-    for i in key_res:
-        ordered_dict.move_to_end(i)
 
-data = OrderedDict(Dustin=29, Anabel=17, Brian=40, Carol=16)
-print(data)
-custom_sort(data, by_values=True)
-print(data)
+def custom_sort(ord_dict, by_values=False):
+    key_res = [sorted(ord_dict), sorted(ord_dict, key=lambda values: ord_dict[values])][by_values]
+    [ord_dict.move_to_end(i) for i in key_res]
 
-# data = OrderedDict(Earth=3, Mercury=1, Mars=4, Venus=2)
+
+# data = OrderedDict(Dustin=29, Anabel=17, Brian=40, Carol=16)
 # print(data)
 # custom_sort(data, by_values=True)
-#
-# print(*data.items())
+# print(data)
+
+data = OrderedDict(Earth=3, Mercury=1, Mars=4, Venus=2)
+print(data)
+custom_sort(data, by_values=False)
+
+print(*data.items())
 
 
 
