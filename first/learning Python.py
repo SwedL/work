43c14070ce6b258e0565490b3494877c5b806b45
AL <swedl@yandex.ru>
2022-10-18 17:47:40 +0500

    Учим Counter часть1

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,22 +1,5 @@
-from collections import OrderedDict
-
-def custom_sort(ordered_dict, by_values=False):
-    key_res = []
-    key_res = [sorted(ordered_dict, key=lambda ord: ord[0]), sorted(ordered_dict, key=lambda ord: ord[1])][by_values]
-    print(key_res)
-    for i in key_res:
-        ordered_dict.move_to_end(i)
-
-data = OrderedDict(Dustin=29, Anabel=17, Brian=40, Carol=16)
-print(data)
-custom_sort(data, by_values=True)
-print(data)
-
-# data = OrderedDict(Earth=3, Mercury=1, Mars=4, Venus=2)
-# print(data)
-# custom_sort(data, by_values=True)
-#
-# print(*data.items())
+from collections import Counter
 
 
+counter = Counter(input().split(','))
 
