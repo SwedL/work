62c09b427469aafd5dd7abe876717d686a07c275
AL <swedl@yandex.ru>
2022-10-20 19:11:24 +0500

    Учим Counter часть2

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,12 +1,14 @@
 
 from collections import Counter
 
-data = Counter('aksjaskfjsklfjdslkfjajfopewtoieqpwdpqworiiqjskanvmcxbmpewrqopkqwlmdzczmxvmvlnjpjqpkqzxvmbowiqeorewi')
+data = input().split()
+list1 = list(map(lambda x: (x[0], int(x[1])), [[i for i in input().split()] for _ in range(int(input()))]))
+
+list_data = {k: list1[k] for k in data if k in list1}
+print(data)
+print(list1)
+print(list_data)
 
 
-
-data.min_values = lambda p: p == max(data.min_values())
-
-print(data.min_values())
 
 
