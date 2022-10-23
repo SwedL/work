44136923c4c6b8e5d7794e0411fa68c9caf893d1
Алексей <supersega@mail.ru>
2022-10-23 23:20:37 +0500

    учим ChainMap2

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,24 +1,19 @@
 
-from collections import ChainMap, Counter
+from collections import ChainMap
 
 
-bread = {'булочка с кунжутом': 15, 'обычная булочка': 10, 'ржаная булочка': 15}
-meat = {'куриный бифштекс': 50, 'говяжий бифштекс': 70, 'рыбный бифштекс': 40}
-sauce = {'сливочно-чесночный': 15, 'кетчуп': 10, 'горчица': 10, 'барбекю': 15, 'чили': 15}
-vegetables = {'лук': 10, 'салат': 15, 'помидор': 15, 'огурцы': 10}
-toppings = {'сыр': 25, 'яйцо': 15, 'бекон': 30}
-menu = ChainMap(bread, meat, sauce, vegetables, toppings)
+def deep_update(chainmap, key, value):
+    if key in sorted(chainmap.parents.keys()):
+        for i in chainmap.maps[1:]:
+            i[key] = value
+    else:
+        chainmap[key] = value
 
 
-zakaz = input().split(',')
-counter = Counter(zakaz)
 
-indent = max(map(len, zakaz))
-indent_minus = indent+3+len(str(max(counter.values())))
-print(indent_minus)
 
-total = sum([menu[i]*counter[i] for i in counter])
-print(sorted(counter.most_common(), key=lambda x: x[0]))
+chainmap = ChainMap({})
 
-print(total)
+print(deep_update(chainmap, 'city', 'Moscow'))
+
 
