1900f77f055dae7d8d977775157da90dbe68982a
Алексей <supersega@mail.ru>
2022-11-09 13:28:20 +0500

    учим eval и exec

--- first/Muzic.py
+++ first/Muzic.py
@@ -3,6 +3,11 @@ from playsound import playsound
 
 
 list1 = os.listdir('D:/МУЗ_02.10')
+print(list1)
+print(len(list1))
 
-for i in list1[1:]:
-    playsound(f'D:/МУЗ_02.10/{i}')
+for k in list1:
+    os.remove(f'G:/NGH/{k}')
+
+#for i in list1[1:]:
+  #  playsound(f'D:/МУЗ_02.10/{i}')
--- first/learning Python.py
+++ first/learning Python.py
@@ -1,12 +1,10 @@
 
+text = eval(input())
 
-def reduce_fraction(fraction):
-    def nod(x=min(fraction)):
-        if fraction[0] % x == 0 and fraction[1] % x == 0:
-            return tuple(list(map(lambda i: int(i / x), fraction)))
-        else:
-            return nod(x-1)
-    return nod()
+print(eval({list: 'text[-1]', tuple: 'text[0]', set: 'len(text)'}[type(text)]))
 
 
-print(reduce_fraction([80, 120]))
+
+
+
+
