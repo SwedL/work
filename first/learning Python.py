31f18e3aa0d2ab33616208e7ab2ce888d7de5293
Алексей <supersega@mail.ru>
2022-11-14 12:49:03 +0500

    учим аннотация типов данных

--- first/learning Python.py
+++ first/learning Python.py
@@ -4,21 +4,22 @@
 
 
 
-def verification(login, password, success, failure):
-    return
-
-def success(login):
-    print(f'Привет, {login}!')
-
-def failure(login, text):
-    print(f'{login}, попробуйте снова. Ошибка: {text}')
-
-verification('timyrik20', 'Beegeek314', success, failure)
+def matrix_to_dict(matrix: list[list]) -> dict[int, list[int | float]]:
+    return {(i+1): matrix[i] for i in range(len(matrix))}
 
 
 
+matrix = [[5, 6, 7], [8, 3, 2], [4, 9, 8]]
 
+print(matrix_to_dict(matrix))
 
+matrix = [[5.1, 6, 7.94]]
 
+print(matrix_to_dict(matrix))
 
+annotations = matrix_to_dict.__annotations__
+
+print(annotations['return'])
+
+print(*matrix_to_dict.__annotations__.values())
 
