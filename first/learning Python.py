bc8d9dd4f45c4b71d570ebbc4c63ea610ab59176
AL <swedl@yandex.ru>
2022-11-14 18:08:51 +0500

    Учим декораторы

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,25 +1,27 @@
 
 
 
+def sandwich(func):
+    def wrapper(*args, **kwargs):
+        up = '---- Верхний ломтик хлеба ----'
+        down = '---- Нижний ломтик хлеба ----'
+        print(up)
+        return func(*args, **kwargs)
+        print(down)
+        return
+    return wrapper
 
 
+@sandwich
+def add_ingredients(ingredients):
+    print(' | '.join(ingredients))
 
-def matrix_to_dict(matrix: list[list]) -> dict[int, list[int | float]]:
-    return {(i+1): matrix[i] for i in range(len(matrix))}
+add_ingredients(['томат', 'салат', 'сыр', 'бекон'])
 
 
+@sandwich
+def beegeek():
+    return 'beegeek'
 
-matrix = [[5, 6, 7], [8, 3, 2], [4, 9, 8]]
 
-print(matrix_to_dict(matrix))
-
-matrix = [[5.1, 6, 7.94]]
-
-print(matrix_to_dict(matrix))
-
-annotations = matrix_to_dict.__annotations__
-
-print(annotations['return'])
-
-print(*matrix_to_dict.__annotations__.values())
-
+print(beegeek())
--- first/main.py
+++ first/main.py
@@ -1,6 +1,23 @@
-import pickle
 
 
-with open('D:/file.pkl', 'rb') as inp_file:
-    obj = pickle.load(inp_file)
-    print(obj)
+
+def sort_priority(values, group):
+    values.sort()
+    values.sort(key=lambda x: x in group, reverse=True)
+
+
+numbers = [150, 200, 300, 1000, 50, 20000]
+sort_priority(numbers, [300, 100, 200])
+
+print(numbers)
+
+numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+sort_priority(numbers, (300, 100, 200))
+
+print(numbers)
+
+numbers = [8, 3, 1, 2, 5, 4, 7, 6]
+group = {5, 7, 2, 3}
+sort_priority(numbers, group)
+
+print(numbers)
