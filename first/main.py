c66d52523048d7b8bb354e2eb74d011297252c93
Алексей <supersega@mail.ru>
2022-12-02 13:22:45 +0500

    учим itertool

--- first/main.py
+++ first/main.py
@@ -2,9 +2,29 @@
 
 
 
-def around(numbers):
-    pass
+from itertools import starmap
 
 
-numbers = [1, 2, 3, 4, 5]
-print(*around(numbers))
+def roundrobin(*args):
+    list1 = []
+    #list2 =
+    # for i in args:
+    #     starmap(lambda k: list1.append(k), list(enumerate(i)))
+    #     print([enumerate(i)])
+    #     # for k in enumerate(i):
+    #     #     list1.append(k)
+    [list1.append(k) for k in enumerate(i for i in args)]
+    list1.sort(key=lambda x: x[0])
+    print(list1)
+    return starmap(lambda n, c: c, list1)
+
+
+
+
+print(*roundrobin('abc', 'd', 'ef'))
+
+numbers = [1, 2, 3]
+letters = iter('beegeek')
+print(*roundrobin(numbers, letters))
+
+print(list(roundrobin()))
