8e8c8d6c017e087dae8c637b5c7c1b222dac675f
Алексей <supersega@mail.ru>
2022-12-27 12:17:38 +0500

    учим re

--- first/learning Python.py
+++ first/learning Python.py
@@ -11,7 +11,12 @@ regex = r'\B(<[a-z]+.*?>)\B'
 data = sys.stdin.read()
 match = re.findall(regex, data, re.I)
 print(match)
+regex_k = r'<([a-z]+)\s?'
+regex_v = r'\bw+=\B'
 
+for i in match:
 
+    print(re.search(regex_k, i))
+    print(re.findall(regex_v, i))
 
 
