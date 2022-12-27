74167e937913769c0b61da03899dd210d67ff2df
AL <swedl@yandex.ru>
2022-12-27 17:42:36 +0500

    учим re

--- first/learning Python.py
+++ first/learning Python.py
@@ -13,13 +13,16 @@ regex_atr = r'\b(([a-z]+-?)+)='
 dict_res = {}
 
 for i in sorted(match):
+   # print(i)
     teg = re.findall(regex_teg, i)[0]
     atr = re.findall(regex_atr, i)
-    print(teg, atr)
-    dict_res[teg] = dict_res.setdefault(teg, []) + [atr]
+    dict_res[teg] = []
+    for k in atr:
+        print(teg, k)
+        dict_res[teg] = dict_res.get(teg) + [list(k)[0]]
 
-
+print(dict_res)
 # for k, v in dict_res.items():
-#     print(f'{k}:', ', '.join(dict.fromkeys(v)))
+#     print(f'{k}:', ', '.join(sorted(dict.fromkeys(v))))
 
 
