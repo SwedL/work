f1047b160afea171e22b4bd3b6542d0b696422ea
Алексей <supersega@mail.ru>
2022-12-30 14:32:26 +0500

    учим re

--- first/learning Python.py
+++ first/learning Python.py
@@ -2,22 +2,6 @@ import re
 import sys
 
 data = sys.stdin.read()
-match1 = re.findall(r'(<[a-z]+.*?>)', data, re.I)
-match2 = re.findall(r'(<[a-z]+>)', data, re.I)
+result = re.sub(r'"{3}[^"]*"{3}\n', '', data)
 
-regex_teg = r'<([a-z\d]+)\s?'
-regex_atr = r'\b(([a-z]+-?)+)='
-dict_res = {}
-
-for i in sorted(match1):
-    teg = re.findall(regex_teg, i)[0]
-    atr = re.findall(regex_atr, i)
-    for k in atr:
-        dict_res.setdefault(teg, []).append(list(k)[0])
-
-for i in sorted(match2):
-    teg = re.findall(regex_teg, i)[0]
-    dict_res.setdefault(teg, [])
-
-for k, v in sorted(dict_res.items()):
-    print(f'{k}:', ', '.join(sorted(dict.fromkeys(v))))
+print(result)
--- first/main.py
+++ first/main.py
@@ -3,8 +3,23 @@ import re
 
 res, n = input(), ''
 
-res = re.findall(r'(\w+)(\W+)?', res)
-
+while n != res:
+    n = res
+    if re.sub(r' ', r'', res).isdigit():
+        res = re.sub(r'(\d+)\s+\1', r'\1', res)
+    else:
+        res = re.sub(r'\b(\w{2,})\W*\s*\1\b', r'\1', res)
 
 print(res)
 #1 1 1 1 1 22 22 22 22 333 333 333 4444
+
+
+
+# res, n = input(), ''
+#
+# while n != res:
+#     n = res
+#     res = re.sub(r'\b(\w{2,})\W*\s*\1\b', r'\1', res)
+#
+#
+# print(res)
