4b7362ac81cd734b960ee3763a7fa0e9f9e872f3
AL <swedl@yandex.ru>
2022-12-30 15:34:16 +0500

    учим re

--- first/learning Python.py
+++ first/learning Python.py
@@ -3,5 +3,7 @@ import sys
 
 data = sys.stdin.read()
 result = re.sub(r'"{3}[^"]*"{3}\n', '', data)
+result = re.sub(r'\t*#+ .*\n', '', result)
+result = re.sub(r'\s+# .*\n', '\n', result)
 
 print(result)
