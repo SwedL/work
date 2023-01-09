4c15c47c162748d901c1979abd007cad06ed9b33
AL <swedl@yandex.ru>
2023-01-09 17:47:16 +0500

    учим ООП

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,9 +1,34 @@
-import re
-import sys
 
-data = sys.stdin.read()
-result = re.sub(r'"{3}[^"]*"{3}\n', '', data)
-result = re.sub(r'\t*#+ .*\n', '', result)
-result = re.sub(r'\s+# .*\n', '\n', result)
+from itertools import permutations
 
-print(result)
+class TriangleChecker():
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def is_triangle(self):
+        if all(map(lambda y: isinstance(y, (int | float)))) or any(map(lambda x: x <= 0, [self.a, self.b, self.c])):
+            return 1
+        elif not all(map(lambda x: (x[0] + x[1]) > x[2], permutations([self.a, self.b, self.c]))):
+            return 2
+        return 3
+
+
+a, b, c = map(int, input().split())
+tr = TriangleChecker(a, b, c)
+print(tr.is_triangle())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- first/main.py
+++ first/main.py
@@ -1,17 +1,9 @@
-import re
+from itertools import permutations
 
 
-res, n = input(), ''
+all_num_permutations = all(map(lambda x: (x[0] + x[1]) > x[2], permutations([self.a, self.b, self.c])))
+print(list(all_num_permutations))
 
-while n != res:
-    n = res
-    if re.sub(r' ', r'', res).isdigit():
-        res = re.sub(r'(\d+)\s+\1', r'\1', res)
-    else:
-        res = re.sub(r'\b(\w{2,})\W*\s*\1\b', r'\1', res)
-
-print(res)
-#1 1 1 1 1 22 22 22 22 333 333 333 4444
 
 
 
