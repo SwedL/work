9bc3a976eeb68800fa5139dc6c0836baca319ebd
Алексей <supersega@mail.ru>
2023-01-10 12:48:58 +0500

    учим ООП

--- first/learning Python.py
+++ first/learning Python.py
@@ -3,11 +3,16 @@ import random
 
 class Cell:
     def __init__(self, mine, around_mines=0):
-        self.mine = choice.Random(0, 1))
+        self.mine = random.Random([True, False])
         self.around_mines = around_mines
         self.fl_open = False
 
 class GamePole:
     def __init__(self, N, M):
         self.N = [Cell()]
-    pass
+
+    def init():
+        pass
+
+    def show():
+        pass
