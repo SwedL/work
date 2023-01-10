9b0287a37cdf1f1e7613187928ac03ae15aedb5a
Алексей <supersega@mail.ru>
2023-01-10 12:46:15 +0500

    учим ООП

--- first/learning Python.py
+++ first/learning Python.py
@@ -1,34 +1,13 @@
-
-from itertools import permutations
-
-class TriangleChecker():
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-
-    def is_triangle(self):
-        if all(map(lambda y: isinstance(y, (int | float)))) or any(map(lambda x: x <= 0, [self.a, self.b, self.c])):
-            return 1
-        elif not all(map(lambda x: (x[0] + x[1]) > x[2], permutations([self.a, self.b, self.c]))):
-            return 2
-        return 3
+import random
 
 
-a, b, c = map(int, input().split())
-tr = TriangleChecker(a, b, c)
-print(tr.is_triangle())
+class Cell:
+    def __init__(self, mine, around_mines=0):
+        self.mine = choice.Random(0, 1))
+        self.around_mines = around_mines
+        self.fl_open = False
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+class GamePole:
+    def __init__(self, N, M):
+        self.N = [Cell()]
+    pass
--- first/main.py
+++ first/main.py
@@ -1,17 +1,15 @@
 from itertools import permutations
 
 
-all_num_permutations = all(map(lambda x: (x[0] + x[1]) > x[2], permutations([self.a, self.b, self.c])))
-print(list(all_num_permutations))
+
+y = int(input())
+
+if isinstance(y, int | float):
+    print('int или float', type(y), y)
+else:
+    print('не int и не float')
 
 
 
 
-# res, n = input(), ''
-#
-# while n != res:
-#     n = res
-#     res = re.sub(r'\b(\w{2,})\W*\s*\1\b', r'\1', res)
-#
-#
-# print(res)
+
